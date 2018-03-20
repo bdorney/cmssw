0f89d982d8b589f3f429831a18976eae656355f3
Brian Dorney <brian.l.dorney@cern.ch>
2018-03-20 16:48:53 +0100

    updating python utilities

--- LFVAnalysis/LFVUtilities/python/utilities.py
+++ LFVAnalysis/LFVUtilities/python/utilities.py
@@ -1,10 +1,72 @@
+import os
+
+# Selection Levels
 selLevels = (
         "all",
-        "id",
-        "idIso"
+        "kin",
+        "kinId",
+        "kinIdIso"
         )
 
+# Monte Carlo Levels
 mcLevels = (
-        "gen"
+        "gen",
         "reco"
         )
+
+# Supported Operators
+supOperators = (
+        "eq",   #equals
+        "fabs", #absolute value
+        "g",    #greater than
+        "ge",   #greater or equal too
+        "l",    #less than
+        "le"    #less than or equal too
+        )
+
+def passesCut(valOfInterest, cutVal, listOfCutStrings):
+    """
+    Checks if valOfInterest passes cutVal uses logical operations
+    defined in listOfCutStrings
+
+    valOfInterest   - numeric value to be checked if passes cut
+    cutVal          - value to compare valOfInterest against
+    listOfCutStrings- list of strings to form a logical comparison with.
+                      Note that elements must be in supOperators and 
+                      length must be <= 2.
+    
+    Returns True (False) if valOfInterest passes (fails) listOfCutStrings
+    """
+
+    if len(listOfCutStrings) > 2:
+        print "list of cut strings longer than expected, given:"
+        print ""
+        print listOfCutStrings
+        print ""
+        print "exiting"
+        exit(os.EX_USAGE)
+
+    for operator in listOfCutStrings:
+        if operator not in supOperators:
+            print "Operator %s not understood"
+            print "The list of supported operators is:"
+            print ""
+            print supOperators
+            print ""
+            print "exiting"
+            exit(os.EX_USAGE)
+
+    if 'fabs' in listOfCutStrings:
+        valOfInterest = abs(valOfInterest)
+        listOfCutStrings.remove('fabs')
+
+    if "eq" in listOfCutStrings:
+        return (valOfInterest == cutVal)
+    elif "g" in listOfCutStrings:
+        return (valOfInterest > cutVal)
+    elif "ge" in listOfCutStrings:
+        return (valOfInterest >= cutVal)
+    elif "l" in listOfCutStrings:
+        return (valOfInterest < cutVal)
+    elif "le" in listOfCutStrings:
+        return (valOfInterest <= cutVal)
